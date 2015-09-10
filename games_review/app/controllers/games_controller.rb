569f8204fb5803273535ff48235e000be7eb9ae8
Yao Yuan <opal_obsidian@hotmail.com>
2015-09-10 19:22:44 +0100

    added routes

--- games_review/app/controllers/games_controller.rb
+++ games_review/app/controllers/games_controller.rb
@@ -1,2 +1,24 @@
 class GamesController < ApplicationController
+
+  def index
+    @games = Game.all
+  end
+
+  def new
+  end
+
+  def create
+    @game = Game.new(params[:game])
+    if @game.save
+      redirect("/games/#{@game.id}")
+    else
+      render :new
+    end
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+
 end
